280b650eea4643cd469f2015fbbf4c8e036f90c2
Evan Fraley <30992695+SplitTrix@users.noreply.github.com>
2021-08-30 20:13:34 +0000

    Made Lab3A

--- Lab3A.cs
+++ Lab3A.cs
@@ -4,6 +4,12 @@ class Lab3A
 {
   public static void Main(string[] args)
   {
-    Console.WriteLine("Hello world!");
+    Console.Write("Amount owed: $");
+    float fpCCBalance = Convert.ToSingle(Console.ReadLine());
+    Console.Write("APR: ");
+    float fpAPR = Convert.ToSingle(Console.ReadLine());
+    Console.WriteLine("Monthly percentage rate: " + fpAPR / 12);
+    fpAPR /= 100; // Converts percent to decimal form for calculation
+    Console.WriteLine("Minimum payment: " + (fpCCBalance * fpAPR) / 12);
   }
 }
